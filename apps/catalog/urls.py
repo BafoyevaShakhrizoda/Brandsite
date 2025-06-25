c7ca8983cf4312a24e30249b4b9d7101ad57ee28
Shahrizoda <saidovashaxnoza092@gmail.com>
2025-06-25 20:33:59 +0500

    update requiremeents.txt

--- apps/catalog/urls.py
+++ apps/catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from . import views
 urlpatterns = [
     path('categories/', views.category_list, name='category_list'),
     path('categories/<slug:slug>/', views.category_detail, name='category_detail'),
